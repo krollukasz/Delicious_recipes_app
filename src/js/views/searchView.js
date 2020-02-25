f5ea493e7fccd05794900bebbbbb15c4c5690ff4
Łukasz Król <lionel@o2.pl>
2020-02-25 21:10:30 +0100

    Add method to clear results, fix results title display

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -6,6 +6,25 @@ export const clearInput = () => {
   elements.searchInput.value = ""
 };
 
+export const clearResults = () => {
+  elements.searchResultsList.innerHTML = "";
+};
+
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+
+    // return the result
+    return `${newTitle.join(" ")}...`;
+  }
+}
+
 const renderRecipe = recipe => {
   const markup = `
     <li>
@@ -14,7 +33,7 @@ const renderRecipe = recipe => {
           <img src="${recipe.image_url}" alt="${recipe.title}">
         </figure>
         <div class="results__data">
-          <h4 class="results__name">${recipe.title}</h4>
+          <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
           <p class="results__author">${recipe.publisher}</p>
         </div>
       </a>
