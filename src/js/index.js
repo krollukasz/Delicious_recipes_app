37dcbd615cbab16162ddd66195219190a345b9c5
Łukasz Król <lionel@o2.pl>
2020-03-08 19:40:55 +0100

    Add recipe view

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,8 @@
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
-import { elements, renderLoader, clearLoader } from "./views/base";
+import * as recipeView from "./views/recipeView";
 import * as searchView from "./views/searchView";
+import { elements, renderLoader, clearLoader } from "./views/base";
 import "../sass/style.scss";
 
 /* Global state of the app
@@ -69,6 +70,11 @@ const controlRecipe = async () => {
 
   if (id) {
     // Prepare UI for changes
+    recipeView.clearRecipe();
+    renderLoader(elements.recipe);
+
+    // Highlight selected search item
+    if (state.search) searchView.highlightSelected(id);
 
     // Create new recipe object
     state.recipe = new Recipe(id);
@@ -83,7 +89,8 @@ const controlRecipe = async () => {
       state.recipe.calcServings();
  
       // Render recipe
-      console.log(state.recipe);
+      clearLoader();
+      recipeView.renderRecipe(state.recipe);
     } catch (error) {
       alert("Error processing recipe !");
     };    
--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -33,6 +33,7 @@ export default class Recipe {
   parseIngredients() {
     const unitsLong = ["tablespoons", "tablespoon", "ounces", "ounce", "teaspoons", "teaspoon", "cups", "pounds"];
     const unitsShort = ["tbsp", "tbsp", "oz", "oz", "tsp", "tsp", "cup", "pound"];
+    const units = [...unitsShort, "g", "kg"];
 
     const newIngredients = this.ingredients.map(el => {
       // 1) Uniform units
@@ -46,7 +47,7 @@ export default class Recipe {
 
       // 3) Parse ingredients into count, unit and ingredient
       const arrayIngredient = ingredient.split(" ");
-      const unitIndex = arrayIngredient.findIndex(element => unitsShort.includes(element));
+      const unitIndex = arrayIngredient.findIndex(element => units.includes(element));
 
       let objIngredient;
       if (unitIndex > -1) {
