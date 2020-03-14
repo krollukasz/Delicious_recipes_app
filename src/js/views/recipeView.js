89171a5dcc5ac30293b3208e7c25f8cc8c0e6547
Łukasz Król <lionel@o2.pl>
2020-03-14 16:53:57 +0100

    Fix icons

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -27,9 +27,9 @@ const formatCount = count => {
 
 const createIngredient = ingredient => `
   <li class="recipe__item">
-    <svg class="recipe__icon">
-      <use href="../src/images/icons.svg#icon-check"></use>
-    </svg>
+    <div class="recipe__icon">
+      <i class="fas fa-check"></i>
+    </div>
     <div class="recipe__count">${formatCount(ingredient.count)}</div>
     <div class="recipe__ingredient">
       <span class="recipe__unit">${ingredient.unit}</span>
@@ -49,16 +49,16 @@ export const renderRecipe = (recipe, isLiked) => {
     
     <div class="recipe__details">
       <div class="recipe__info">
-        <svg class="recipe__info--icon">
-          <use href="../src/images/icons.svg#icon-stopwatch"></use>
-        </svg>
+        <div class="recipe__info--icon">
+          <i class="far fa-clock"></i>
+        </div>
         <span class="recipe__info--data recipe__info--data--minutes">${recipe.time}</span>
         <span class="recipe__info-text"> minutes</span>
       </div>
       <div class="recipe__info">
-        <svg class="recipe__info--icon">
-          <use href="../src/images/icons.svg#icon-man"></use>
-        </svg>
+        <div class="recipe__info--icon">
+          <i class="fas fa-male"></i>
+        </div>
         <span class="recipe__info--data recipe__info--data--people">${recipe.servings}</span>
         <span class="recipe__info-text"> servings</span>
         
@@ -81,9 +81,9 @@ export const renderRecipe = (recipe, isLiked) => {
         ${recipe.ingredients.map(el => createIngredient(el)).join("")}      
       </ul>
       <button class="btn-small recipe__btn recipe__btn--add">
-        <svg class="search__icon">
-          <use href="../src/images/icons.svg#icon-shopping-cart"></use>
-        </svg>
+        <div class="search__icon">
+          <i class="fas fa-shopping-cart"></i>
+        </div>
         <span>Add to shopping list</span>
       </button>
     </div>
@@ -96,9 +96,9 @@ export const renderRecipe = (recipe, isLiked) => {
       </p>
       <a class="btn-small recipe__btn" href="${recipe.url}" target="_blank">
         <span>Directions</span>
-        <svg class="search__icon">
-          <use href="../src/images/icons.svg#icon-triangle-right"></use>
-        </svg>
+        <div class="search__icon">          
+          <i class="fas fa-angle-right"></i>
+        </div>
       </a>
     </div>
   `;
